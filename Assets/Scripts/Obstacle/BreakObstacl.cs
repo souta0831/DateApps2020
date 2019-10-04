1ad927d25c68f5c38e938ccd99d36a38ddc71f77
souta_notePC <darkwind0831@gmail.com>
2019-10-04 14:39:51 +0900

    壊れるオブジェクトを実装(エフェクトは作ってません)

---
+++ Assets/Scripts/Obstacle/BreakObstacl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakObstacl : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _break_efect;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ListInstance();
+            if (collision.gameObject.GetComponent<Player>().GetState() == PlayerStateID.SLIDING)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+    private void ListInstance()
+    {
+        for (int i = 0; i < _break_efect.Count; i++)
+        {
+            if (_break_efect[i] != null)
+            {
+                Instantiate(_break_efect[i], transform.position, transform.rotation);
+            }
+
+        }
+    }
+}
