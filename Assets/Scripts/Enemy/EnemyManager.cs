2bc940612b6860bfbcdd019ad08f055d4b018059
souta_desktop <souta130831@yahoo.co.jp>
2019-10-07 00:17:12 +0900

    エネミー管理クラス・エネミークラス・弾丸クラスを作成

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _player;
+    [SerializeField]
+    List<Enemy> EnemyList;
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.GetComponent<Enemy>() != null) {
+                EnemyList.Add(child.gameObject.GetComponent<Enemy>());
+            }
+        }
+        foreach (Enemy enemy_list in EnemyList)
+        {
+            enemy_list.SetPlayer(_player.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
