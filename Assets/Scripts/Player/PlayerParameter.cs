3fb7de447ba173e8de29576fc4dc8aecc5b6aff6
souta_desktop <souta130831@yahoo.co.jp>
2019-10-20 23:35:55 +0900

    プレイヤーをパラメーターでいじれるようにしました　ロックオン機能をプレイヤーのスクリプトのみ実装

---
+++ Assets/Scripts/Player/PlayerParameter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "custom/PlayerController", fileName = "PlayerParameter")]
+
+public class PlayerParameter : ScriptableObject
+{
+    [SerializeField]
+    public float RunSpeed = 3.0f;
+    [SerializeField]
+    public float JumpPower = 100.0f;
+    [SerializeField]
+    public float SlidingSpeed = 6.0f;
+    [SerializeField]
+    public float SlidingLRSpeed = 2.0f;
+
+}
