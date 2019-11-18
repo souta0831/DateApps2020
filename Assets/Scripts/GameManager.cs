4c039a97f437548a26469e2184e1d44aef6f58c4
souta_notePC <darkwind0831@gmail.com>
2019-11-18 16:19:55 +0900

    ボスが降ってくるときにカメラワークを変更するようにしました。

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 //進行管理クラス
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Camera _MainCamera;
+    static private Camera _mainCamera= default;
+    static private Camera _nowCamera=default;
+    static private int _now_disply = 0;
+    public Camera MainCamera
+    {
+        get { return _mainCamera; }
+    }
     void Awake()
     {
         InputController.SetDefault();
+        //子供からカメラを取得する
+        _mainCamera = _MainCamera;
+        //初期カメラをメインカメラに設定   
+        GameManager.ChangeCamera(_mainCamera);
 
     }
 
@@ -16,5 +28,36 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         InputController.Update();
+        CameraCheck();
     }
+    void CameraCheck()
+    {
+        if (_nowCamera == null)
+        {
+            ChangeCamera(null);
+        }
+    }
+    public static void ChangeCamera(Camera camera)
+    {
+        if (camera == null)
+        {
+            if(_nowCamera!=null) _nowCamera.enabled = false;
+            _nowCamera = _mainCamera.GetComponent<Camera>();//_mainCamera;
+            _nowCamera.enabled = true;
+
+            return;
+        }
+        if (camera!=_nowCamera)
+        {
+            if(_nowCamera!=null) _nowCamera.enabled = false;
+            //カメラを取得する
+            _nowCamera = camera.GetComponent<Camera>();
+            _nowCamera.enabled = true;
+
+            return;
+        }
+
+
+    }
+    
 }
