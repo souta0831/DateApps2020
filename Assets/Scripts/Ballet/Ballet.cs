99cad8fc09390782318d04e6b2b751ad40091657
souta_notePC <darkwind0831@gmail.com>
2019-11-22 18:00:28 +0900

    no message

--- Assets/Scripts/Ballet/Ballet.cs
+++ Assets/Scripts/Ballet/Ballet.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Ballet : BalletBase
 {
-
+    
     protected override void Initialize()
     {
         //ぶれを求める
         Vector3 accuracy = new Vector3(Random.Range(-_accuracy, _accuracy), 0, Random.Range(-_accuracy, _accuracy));
         //ターゲットのほうに弾を向ける
-        transform.LookAt(_targetObject.transform.position + accuracy);
+        transform.LookAt(_targetObject.transform.position);
 
     }
     protected override void Move()
